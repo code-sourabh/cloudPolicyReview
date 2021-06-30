03ab9d3805de5c1491c228f881f2058e4f3da541
sourabh <sourabhmishra1262@gmail.com>
2021-07-01 01:37:47 +0530

    employee and manager policy webpage demo done

--- cprs/managerOrg/urls.py
+++ cprs/managerOrg/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('' , views.manager_index , name='manager_index'),
+    path('employee_policy_review.html/' , views.employee_policy_review , name='employee_policy_review')
 ]
--- cprs/managerOrg/views.py
+++ cprs/managerOrg/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def manager_index(request):
-    return HttpResponse("<h2>This is my manager page... </h2>")
+# return HttpResponse("<h2>This is my manager page... </h2>")
 # Create your views here.
+    return render(request , 'managerOrg/manager_index.html')
+
+def employee_policy_review(request):
+    return render(request , 'managerOrg/employee_policy_review.html')
